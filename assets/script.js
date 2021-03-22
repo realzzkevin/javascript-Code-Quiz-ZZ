890085eb25ba01390ee9f91971263d1c59b16a2d
zzkevin <realzzkevin@gmail.com>
2021-03-21 22:49:49 -0400

    create index

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+var highScores = [];
+
+// all quiz questions will be store in arry quizQuestions.
+var quizQuestions = [];
+//sample question object.
+quizQuestions[0] = {
+
+    question : "who invented JavaScript",
+
+    answers : ["Douglas crockford", "Sheryl Sandberg", "Brendan Eich"],
+
+    CorrectAnswer : "Brendan Eich"
+};
+
+quizQuestions[1] = {
+
+    question : "inside with HTML elemment do we put the JavaScript?",
+
+    answers : ["<javascript>", "<header>", "<body>", "<script>"],
+
+    CorrectAnswer : "<script>"
+};
+
+
+console.log(quizQuestions);
+
+
+function timer (){
+
+}
+
+function dispalyQuestion(question) {
+
+}
+
+
+function showHighScores() {
+
+}
+
+function startQuiz(){
+
+}
+
+startQuiz();
