57cae8a45c6d384c5624dfd72867927e134b4b8f
zzkevin <realzzkevin@gmail.com>
2021-03-23 13:06:55 -0400

    update javascript code

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 var highScores = [];
 var currentScore = 0;
 var remainSeconds = 60;
+var questions = document.getElementById('questions');
+var choices = document.getElementById('choices');
+var status = document. getElementById('status');
+var displayScore = document.getElementById('highScores');
 
 // all quiz questions will be store in arry quizQuestions.
 var quizQuestions = [];
@@ -17,9 +21,9 @@ quizQuestions[0] = {
 quizQuestions[1] = {
 
     question : "inside with HTML elemment do we put the JavaScript?",
-
+    
     answers : ["<javascript>", "<header>", "<body>", "<script>"],
-
+    
     CorrectAnswer : "<script>"
 };
 
@@ -28,48 +32,99 @@ console.log(quizQuestions);
 
 
 function timer (){
-
+    
     remainSeconds = 60;
+
+    var counter = document.getElementById("timeCounter");
+
     
     var timeInterval = setInterval(function () {
-
+        
         remainSeconds--;
 
+        
         if(remainSeconds ===0) {
-
+            
             clearInterval(timeInterval);
-
+            
             quizOver();
-
+            
         }
-
+        
     }, 1000);
-
+    
 }
 
 function dispalyQuestion (question) {
-
-
+    
+    
 }
 
 function quizOver (str) {
-
+    
 }
 
 function showHighScores () {
-
+    event.preventDefault();
+    clearPage();
+    
 }
 
 function saveScores () {
-
+    
 }
 
-function initPage () {
+function clearPage () {
+    event.stopPropagation();
+    
+    while (questions.firstChild){
+        questions.removeChild(questions.firstChild);
+    }
 
+    while (choices.firstChild){
+        choices.removeChild(choices.firstChild);
+    }
+
+    while (status.firstChild){
+        status.removeChild(choices.firstChild);
+    }
+ 
 }
 
 function startQuiz(){
+    event.stopPropagation();
+    clearPage();
+    timer();
+}
 
+
+
+function initPage () {
+
+    var title = document.createElement('h1');
+    var intro = document.createElement('p');
+    var startBtn = document.createElement('button');
+    
+
+    title.textContent = "this is the question title";
+    intro.textContent = "do you want to start the quiz?";
+    startBtn.textContent = "start";
+
+    questions.appendChild(title);
+    choices.appendChild(intro);
+    choices.appendChild(startBtn);
+
+    console.log(choices.childNodes);
+
+    startBtn.addEventListener('click', startQuiz);
+
+    displayScore.addEventListener('click', showHighScores);
 }
 
 initPage();
+
+
+
+
+
+//
